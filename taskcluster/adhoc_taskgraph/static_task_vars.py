232f7fdd0761373b9c364eb47fc181fff1dde949
Heitor Neiva <hneiva@mozilla.com>
2022-06-20 09:52:16 -0700

    Change VPN URLs to point to VPN repo

--- taskcluster/adhoc_taskgraph/static_task_vars.py
+++ taskcluster/adhoc_taskgraph/static_task_vars.py
@@ -7,8 +7,7 @@ MAC_STATIC_VARS_BY_PRODUCT = {
     # These variables will always be included in the task payload
     "mozillavpn": {
         # URLs used for Mac Notarization
-        # TODO: These URLs should at least target the VPN repo
-        "loginItems-entitlements-url": "https://gist.githubusercontent.com/bhearsum/d4dc4b93045baa3698c3b0b55e2491bb/raw/f3649e1b703aefe833916a71b725062dd1b7c86e/gistfile1.txt",
-        "entitlements-url": "https://gist.githubusercontent.com/bhearsum/5dac6f523a828675bbc00e8165e29946/raw/6b5a395c476679bcc5ecb07eafdc8e5adefc7470/gistfile1.txt",
+        "loginItemsEntitlementsUrl": "https://raw.githubusercontent.com/mozilla-mobile/mozilla-vpn-client/main/taskcluster/scripts/signing/loginItems-entitlements.xml",
+        "entitlementsUrl": "https://raw.githubusercontent.com/mozilla-mobile/mozilla-vpn-client/main/taskcluster/scripts/signing/entitlements.xml",
     }
 }
